e2371106f78cbe99271941edb1f4de1030d4fd65
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-02-24 17:03:09 +0100

    feat: enhance display output #8

--- src/cts_cli/api/departure_time.py
+++ src/cts_cli/api/departure_time.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 """Module for all departure time functions related."""
-from datetime import datetime
+from datetime import datetime, timezone
+import math
 
 import click
 import requests
 
-ENDPOINT = "/estimated-timetable"
+ESTIMATED_TIMETABLE_ENDPOINT = "/estimated-timetable"
+STOP_MONITORING_ENDPOINT = "/stop-monitoring"
 TIMEOUT = 10
 
 
-def departure_time_call(ctx, line: str, station: str, destination: str):
+def departure_time_call(ctx, station: str) -> str:
     """
     Get the departure time for a specific line, station, and destination.
 
@@ -24,32 +26,121 @@ def departure_time_call(ctx, line: str, station: str, destination: str):
     Returns:
         dict: The departure information for the specified line, station, and destination.
     """
-    full_url = f"{ctx.obj.get('url')}{ENDPOINT}"
-    print(full_url)
-    response = requests.get(
-        url=full_url,
+    et_url = f"{ctx.obj.get('url')}{ESTIMATED_TIMETABLE_ENDPOINT}"
+    et_response = requests.get(
+        url=et_url,
         auth=(ctx.obj.get("token"), ctx.obj.get("password")),
         timeout=TIMEOUT,
     )
+    station_ref = get_station_ref(et_response.json(), station)
+
+    sm_url = (
+        f"{ctx.obj.get('url')}{STOP_MONITORING_ENDPOINT}?MonitoringRef={station_ref}"
+    )
+    sm_response = requests.get(
+        url=sm_url,
+        auth=(ctx.obj.get("token"), ctx.obj.get("password")),
+        timeout=TIMEOUT,
+    )
+    return get_station_departures(sm_response.json())
+
+
+def get_station_ref(json_response: dict, station_name: str) -> str:
+    """
+    Get the reference ID of a station from the JSON response.
+
+    Args:
+        json_response (dict): The JSON response containing the station information.
+        station_name (str): The name of the station.
+
+    Returns:
+        str: The reference ID of the station.
+
+    Raises:
+        KeyError: If no estimated journey version frame is found in the JSON response.
+
+    Examples:
+        >>> json_response = {"ServiceDelivery": {"EstimatedTimetableDeli...}]}}
+        >>> get_station_ref(json_response, "StationA")
+        '12345'
+    """
     try:
-        estimated_journey_version_frame = response.json()["ServiceDelivery"][
+        estimated_journey_version_frame = json_response["ServiceDelivery"][
             "EstimatedTimetableDelivery"
-        ][0]["EstimatedJourneyVersionFrame"]
+        ][0]["EstimatedJourneyVersionFrame"][1:]
     except KeyError as e:
         click.echo(f"Error. No estimated journey version frame found: {str(e)}")
-    for journey_frame in estimated_journey_version_frame:
-        line_station = [
-            journey
-            for journey in journey_frame["EstimatedVehicleJourney"]
-            if journey["LineRef"] == line
-        ]
+    for estimated_journey in estimated_journey_version_frame:
+        for journey in estimated_journey["EstimatedVehicleJourney"]:
+            line_station = [
+                station
+                for station in journey["EstimatedCalls"]
+                if station["StopPointName"].lower() == station_name.lower()
+            ]
         if line_station:
             break
-    datetime_str = [
-        call
-        for call in line_station[0]["EstimatedCalls"]
-        if call["StopPointName"].lower() == station.lower()
-        and call["DestinationName"].lower() == destination.lower()
-    ][0]["ExpectedDepartureTime"]
+    return line_station[0]["StopPointRef"]
 
-    return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S%z")
+
+def get_station_departures(json_response: dict) -> list:
+    """
+    Get the list of departures for a station from the JSON response.
+
+    Args:
+        json_response (dict): The JSON response containing the station departures.
+
+    Returns:
+        list: A list of dictionaries representing the departures, \
+each containing the monitoring reference,
+        line, destination, and station information.
+
+    Examples:
+        >>> json_response = {"ServiceDelivery": {"StopMonitoringDelivery": \
+[{"MonitoredStopVisit": ...}]}}
+        >>> get_station_departures(json_response)
+        [
+            {
+                "monitoring_ref": "12345",
+                "line": "LineA",
+                "destination": "DestinationA",
+                "station": "StationA"
+            },
+            ...
+        ]
+    """
+    monitored_stop_visits = json_response["ServiceDelivery"]["StopMonitoringDelivery"][
+        0
+    ]["MonitoredStopVisit"]
+    return [
+        [
+            stop["MonitoredVehicleJourney"]["LineRef"],
+            stop["MonitoredVehicleJourney"]["DestinationName"],
+            stop["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedDepartureTime"],
+            get_remaining_minutes(
+                stop["MonitoredVehicleJourney"]["MonitoredCall"][
+                    "ExpectedDepartureTime"
+                ]
+            ),
+        ]
+        for stop in monitored_stop_visits
+    ]
+
+
+def get_remaining_minutes(given_datetime_str: str) -> str:
+    """
+    Get the remaining minutes between the given datetime and the current datetime.
+
+    Args:
+        given_datetime_str (str): The given datetime string in ISO format.
+
+    Returns:
+        str: The remaining minutes as a string.
+
+    Examples:
+        >>> get_remaining_minutes("2022-01-01T12:00:00")
+        '123 min'
+    """
+    given_datetime = datetime.fromisoformat(given_datetime_str)
+    current_datetime = datetime.now(timezone.utc)
+    time_difference = (given_datetime - current_datetime).total_seconds() / 60
+    return f"{math.ceil(time_difference)} min" if time_difference > 1 else "Close"
